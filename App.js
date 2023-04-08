3a93dc918af37118e3362e350b9f57b3b0ec90a6
devanshi00 <devanshi7309@gmail.com>
2023-04-08 18:48:02 +0530

    Event Handler added

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import IntroScreen from './views/IntroScreen';
+import MainScreen from './views/MainScreen';
+
 
 export default function App() {
+  const [isloaded, setIsloaded] = useState(false);
+  setTimeout(() => {
+    setIsloaded(true);
+  }, 2000)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
       <StatusBar style="auto" />
+      {isloaded ? <MainScreen /> : <IntroScreen />}
+      {/* <MainScreen /> */}
     </View>
   );
 }
@@ -13,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ views/MainScreenStyle.js
@@ -0,0 +1,90 @@
+export const main_screen = {
+    marginTop: 30,
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+};
+export const main_screen__display = {
+    elevation: 10,
+    width: '95%',
+    // height: '25%',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    display: 'flex',
+
+    // alignItems: 'flex-end',
+    // justifyContent: 'flex-end',
+    marginBottom: 10,
+    padding: 10,
+};
+export const main_screen__display_text = {
+    fontSize: 50,
+    textAlign: 'right',
+};
+export const main_screen__keypad = {
+    width: '100%',
+    height: '70%',
+    // backgroundColor: '#FF5757',
+    display: 'flex',
+    // justifyContent: 'center',
+};
+export const main_screen__keypad_row = {
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+};
+export const btn_outer = {
+    width: 90,
+    height: 90,
+    backgroundColor: 'white',
+    borderRadius: 90,
+    elevation: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
+};
+export const bg_button = {
+    backgroundColor: 'white',
+    color: 'black',
+    fontSize: 30,
+};
+export const btn1_outer = {
+    width: 90,
+    height: 90,
+    backgroundColor: '#FF5757',
+    borderRadius: 90,
+    elevation: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
+};
+export const bg1_button = {
+    backgroundColor: '#FF5757',
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+};
+export const btn2_outer = {
+    width: 90,
+    height: 90,
+
+    backgroundColor: 'grey',
+    borderRadius: 90,
+    elevation: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
+};
+export const bg2_button = {
+    backgroundColor: 'grey',
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+};
